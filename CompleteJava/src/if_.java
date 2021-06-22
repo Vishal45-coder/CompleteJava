bc5ed6b01665031c3aec220bc809a9261a36bb5e
Vishal45-coder <vishalraavi1999@gmail.com>
2021-06-22 22:01:30 +0530

    Error checking

--- CompleteJava/src/if_.java
+++ CompleteJava/src/if_.java
@@ -8,7 +8,7 @@ public class if_{
 
     
         if(score!=210);{
-            System.out.println("This is score");//Only this line will execute if the stament satisfie
+            System.out.println("This is score");//Only this line will execute if the stament satisfies
             System.out.println("this is also score");//This is executed because there is no codeblock(Doesn't come under if block)    
         }
     }

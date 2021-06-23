4b4804533fa79825836c7facec9fab9f7b354405
Vishal45-coder <vishalraavi1999@gmail.com>
2021-06-23 18:02:47 +0530

    Correcting comments

--- CompleteJava/src/if_.java
+++ CompleteJava/src/if_.java
@@ -3,13 +3,13 @@ public class if_{
     int score=210;
 
     if(score!=210)
-        System.out.println("This is score");//Only this line will execute if the stament satisfies
-        System.out.println("this is also score");//This is executed because there is no codeblock(Doesn't come under if block)
+        System.out.println("This is score");//Only this line will execute if the statement satisfies
+        System.out.println("this is also score");//This is executed because there is no code block(Doesn't come under if block)
 
     
         if(score!=210);{
-            System.out.println("This is score");//Only this line will execute if the stament satisfies
-            System.out.println("this is also score");//This is executed because there is no codeblock(Doesn't come under if block)    
+            System.out.println("This is score");//Only this line will execute if the statement satisfies
+            System.out.println("this is also score");//This is executed because there is no code block(Doesn't come under if block)    
         }
     }
     

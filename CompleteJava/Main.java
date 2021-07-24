ca09256831dc81a6bb55e145118bc9a69467f03b
Vishal-456 <87227517+Vishal-456@users.noreply.github.com>
2021-07-24 14:37:17 +0000

    Update Main.java

--- CompleteJava/Main.java
+++ CompleteJava/Main.java
@@ -3,5 +3,6 @@ class Main {
 	public static void main(String[] args) {
 	System.out.println("hi hello");
 	System.out.println("Created from cloud 2");
+	System.out.println("From github in cloud programming");
 	}
 }

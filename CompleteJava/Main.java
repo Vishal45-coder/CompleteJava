ff3e346971f1f3a7155898e7846918e63909a057
Vishal45-Coder <vishalraavi1999@gmail.com>
2021-07-24 14:32:46 +0000

    From cloud 2

--- CompleteJava/Main.java
+++ CompleteJava/Main.java
@@ -2,6 +2,6 @@ class Main {
 
 	public static void main(String[] args) {
 	System.out.println("hi hello");
-	System.out.println("Created from cloud");
+	System.out.println("Created from cloud 2");
 	}
 }
